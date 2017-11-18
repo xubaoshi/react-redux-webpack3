6b190c0d24b6ea3b2f4bdceadc7318934cb8693b
xubaoshi <948869516@qq.com>
2017-11-18 22:12:08 +0800

    delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-
-console.log('it is test');
-console.log('it is test');
-conxsole.log('it is test');
-conxsole.log('it is test');
-conxsole.log('it is test');
-conxsole.log('it is test');
-conxsole.log('it is test');
-
