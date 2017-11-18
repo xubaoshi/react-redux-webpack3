f53e788ddf7aa340303fde746eb0b560c517c818
xubaoshi <948869516@qq.com>
2017-11-18 22:08:59 +0800

    merge test.js from master branch

--- test.js
+++ test.js
@@ -1,3 +1,9 @@
 
 console.log('it is test');
 console.log('it is test');
+conxsole.log('it is test');
+conxsole.log('it is test');
+conxsole.log('it is test');
+conxsole.log('it is test');
+conxsole.log('it is test');
+
